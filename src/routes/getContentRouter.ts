74e1f849e7d9613c9e67e1b3d61974f088152a68
Dennis Bogdan Aleksandrov <extraordinarycoutesy@gmail.com>
2020-10-11 00:48:22 -0700

    Heroku

--- src/routes/getContentRouter.ts
+++ src/routes/getContentRouter.ts
@@ -3,12 +3,11 @@ import ContentModel from '../model/content'
 const getContentRouter = express.Router()
 
 getContentRouter.get('/getcontentdata', async (req, res, next) => {
-  res.status(200).send({ message: 'fuck yea' })
-  // const data = await ContentModel.find()
-  // if (data[0])
-  //   res.status(200).send(data[0])
-  // else
-  //   res.sendStatus(500)
+  const data = await ContentModel.find()
+  if (data[0])
+    res.status(200).send(data[0])
+  else
+    res.sendStatus(500)
 })
 
 export default getContentRouter
