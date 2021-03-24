c8db35169e97355e405eb6308ce77e8b41faf047
Dennis Bogdan Aleksandrov <extraordinarycoutesy@gmail.com>
2021-03-24 13:43:59 -0700

    rm console logs

--- src/routes/getContentRouter.ts
+++ src/routes/getContentRouter.ts
@@ -4,7 +4,6 @@ const getContentRouter = express.Router();
 
 getContentRouter.get("/getcontentdata", async (req, res, next) => {
   const data = await ContentModel.find();
-  console.log(data);
   if (data[0]) res.status(200).send(data[0]);
   else res.sendStatus(500);
 });
@@ -14,7 +13,6 @@ export default getContentRouter;
 const getContentExampRouter = express.Router();
 getContentExampRouter.get("/getcontentdataexmp", async (req, res, next) => {
   const data = await ContentModel.find();
-  console.log(data);
   if (data[1]) res.status(200).send(data[1]);
   else res.sendStatus(500);
 });
