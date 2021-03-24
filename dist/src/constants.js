be314df1a4741f9bb28c4375c5d550635a0823b7
Dennis Bogdan Aleksandrov <extraordinarycoutesy@gmail.com>
2021-03-24 09:30:32 -0700

    quick save

--- dist/src/constants.js
+++ dist/src/constants.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.resInternalError = exports.incorrectCredentials = void 0;
-exports.incorrectCredentials = (res, mesg) => res.status(404).send({ message: mesg !== null && mesg !== void 0 ? mesg : 'Incorrect username or password' });
-exports.resInternalError = (res, text) => res.status(500).send({ message: text !== null && text !== void 0 ? text : 'Oops. Something went wrong' });
+const incorrectCredentials = (res, mesg) => res.status(404).send({ message: mesg !== null && mesg !== void 0 ? mesg : 'Incorrect username or password' });
+exports.incorrectCredentials = incorrectCredentials;
+const resInternalError = (res, text) => res.status(500).send({ message: text !== null && text !== void 0 ? text : 'Oops. Something went wrong' });
+exports.resInternalError = resInternalError;
 //# sourceMappingURL=constants.js.map
--- dist/src/index.js
+++ dist/src/index.js
@@ -12,18 +12,14 @@ const login_1 = __importDefault(require("./routes/login"));
 const changePassword_1 = __importDefault(require("./routes/changePassword"));
 const passport_1 = __importDefault(require("passport"));
 const express_session_1 = __importDefault(require("express-session"));
-const passport_local_1 = __importDefault(require("passport-local"));
 require("../passportConfig");
 const updateContent_1 = __importDefault(require("./routes/updateContent"));
 const fetchContentDataLoginRouter_1 = __importDefault(require("./routes/fetchContentDataLoginRouter"));
 const getContentRouter_1 = __importDefault(require("./routes/getContentRouter"));
 const postCrystalParallaxRouter_1 = __importDefault(require("./routes/postCrystalParallaxRouter"));
 const getCrystalParallaxRouter_1 = __importDefault(require("./routes/getCrystalParallaxRouter"));
-// ! logging in works and lastly, how do we add passport as middleware to check if user has unexpired access token and if expired then refresh if refresh token exists.
-// ~ implement jwt and disable sessions
 const app = express_1.default();
-const port = process.env.PORT || 8090;
-const defaultValues = { title: 'Hey', message: 'Portfolio CMS' };
+const port = process.env.PORT || 8080;
 app.use(cors_1.default());
 app.use(express_1.default.json());
 app.use(express_session_1.default({
@@ -33,12 +29,7 @@ app.use(express_session_1.default({
 }));
 app.use(passport_1.default.initialize());
 app.use(passport_1.default.session());
-const LocalStrategy = passport_local_1.default.Strategy;
-// ===== Authentication passport strategy
 app.use('/', register_1.default, login_1.default, changePassword_1.default, updateContent_1.default, fetchContentDataLoginRouter_1.default, getContentRouter_1.default, postCrystalParallaxRouter_1.default, getCrystalParallaxRouter_1.default);
-// * encrypt password with hash
-// * make first time signup possible
-// * authenticate password and username && if true, return data, and give access to modify content
 mongoose_1.default.connect(process.env.MONGO_URI, {
     useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true
 });
